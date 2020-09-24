9fc5169d55438cef0e99e9f93cbf970aa2debe31
James Su-Shum <sushumjames@gmail.com>
2020-09-23 23:22:54 -0400

    Created PostCommentViewModel, CommentController with Index method, and Index list view

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TabloidMVC.Models;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _commentRepository;
+        private readonly IPostRepository _postRepository;
+
+        public CommentController(ICommentRepository commentRepository, IPostRepository postRepository)
+        {
+            _commentRepository = commentRepository;
+            _postRepository = postRepository;
+        }
+
+        // GET: Comment/Index/1
+        public ActionResult Index(int Id)
+        {
+            Post post = _postRepository.GetPublishedPostById(Id);
+
+            List<Comment> comments = _commentRepository.GetAllComments(Id);
+
+            PostCommentViewModel vm = new PostCommentViewModel()
+            {
+                Post = post,
+                Comments = comments
+            };
+
+            return View(vm);
+        }
+
+        // GET: CommentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
